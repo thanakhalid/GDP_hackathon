a27fff921099e13ca269124dab19dc57308ac4d3
Hatoon F. Aloqaily <115896985+AloqailyHat@users.noreply.github.com>
2023-09-01 23:34:46 +0300

    Update

--- ui.py
+++ ui.py
@@ -1,13 +1,13 @@
 import streamlit as st
 from ui_utils import check_password
-from pdf_to_quizz import pdf_to_quizz
+#from pdf_to_quizz import pdf_to_quizz
 from text_to_quizz import txt_to_quizz
-from generate_pdf import generate_pdf_quiz
+#from generate_pdf import generate_pdf_quiz
 import json
 
 import asyncio
 
-st.title("PDF to Quiz (:-)(-: ) for grad 5")
+st.title("Generate questions from text!")
 
 def build_question(count, json_question):
 
@@ -31,35 +31,20 @@ def build_question(count, json_question):
                 color = ":red"
                 st.write(f":red[Wrong answer. The correct answer is {rep}].")                
 
-        st.write(f"{color}[Your answer: {selected_answer}]")  # i stopped here
+        st.write(f"{color}[Your answer: {selected_answer}]")  
 
         count += 1
 
     return count
 
 # Upload PDF file
-uploaded_file = st.file_uploader(":female-student:", type=["pdf"])
+# uploaded_file = st.file_uploader(":female-student:", type=["pdf"])
 txt = st.text_area('Type the text from which you want to generate the quiz')
 
 if st.button("Generate Quiz", key=f"button_generer"):
     if txt is not None:
         with st.spinner("Quiz generation..."):
             st.session_state['questions'] = asyncio.run(txt_to_quizz(txt))
-            st.write("Quiz generated successfully!")
-
-if uploaded_file is not None:    
-    old_file_name = st.session_state.get('uploaded_file_name', None)
-    if (old_file_name != uploaded_file.name):
-        # Convert PDF to text
-        with st.spinner("Quiz generation..."):
-
-            with open(f"data/{uploaded_file.name}", "wb") as f:
-                f.write(uploaded_file.getvalue())        
-
-            # Initialize session state
-            st.session_state['uploaded_file_name'] = uploaded_file.name
-            st.session_state['questions'] = asyncio.run(pdf_to_quizz(f"data/{uploaded_file.name}"))
-
             st.write("Quiz generated successfully!")
 
 if ('questions' in st.session_state):
@@ -69,21 +54,4 @@ if ('questions' in st.session_state):
 
         count = build_question(count, json_question)
         
-    # generate pdf quiz
-    if st.button("Generate PDF Quiz", key=f"button_generer_quiz"):
-        with st.spinner("Generation of the quiz in PDF..."):
-            json_questions = st.session_state['questions']
-            # save into a file
-            file_name = uploaded_file.name
-
-            # remove extension .pdf from file name
-            if file_name.endswith(".pdf"):
-                file_name = file_name[:-4]
-
-            with open(f"data/quiz-{file_name}.json", "w", encoding='latin-1', errors='ignore') as f:
-                str = json.dumps(json_questions)
-                f.write(str)
-
-            generate_pdf_quiz(f"data/quiz-{file_name}.json", json_questions)
-            
-            st.write("PDF Quiz successfully generated!")      
+   
